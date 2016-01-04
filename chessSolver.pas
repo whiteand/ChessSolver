b9ce7aa14ee123bd387bc28d59a8948e52c00258
Andrew Beletskiy <andrewbeletskiy@gmail.com>
2016-01-04 16:17:26 +0200

    Закончена основная структура программы: Добавлена структура: ход, массив для ходов, функция перебора всех ходов, условие завершения, функции для проверки на шаг

--- chessSolver.pas
+++ chessSolver.pas
@@ -1,5 +1,9 @@
 Program ChessSolver;
 uses crt;
+type move = record
+              iStart, iEnd, jStart, jEnd, figureStart, figureEnd: integer;
+            end;
+     mas = array [1..10000] of move;
 const n        = 8;
       peshka   = 1;
       loshad   = 2;
@@ -18,8 +22,12 @@ const n        = 8;
 
 
 var field: array [1..n, 1..n] of integer;
+    moves: array [1..10000] of move;
+    countOfPossibleMoves: integer;
+    isCheckToWhite, isCheckToBlack: boolean;
 
-
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 procedure showHelp;
 begin
   textcolor(14);
@@ -43,7 +51,8 @@ begin
   writeln('ferz:     ', -ferz);
   writeln('korol:    ', -korol);
 end;//End showHelp
-
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 procedure findKorol(color: integer; var i0,j0: integer);
 var founded: boolean;
     i,j: integer;
@@ -64,10 +73,8 @@ begin
     if (founded) then break;
   end;
 end;
-
-
-
-
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 procedure initialize;
 var fName : string;
     i,j   : integer;
@@ -95,9 +102,12 @@ begin
     field[i,j] := figure;
   end;
   close(f);
+  countOfPossibleMoves := 0;
+
   clrscr;
 end;
-//Reshenie
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 function getFigureOn(i,j: integer): integer;
 begin
   if (i>=1) and (i<=n) and (j>=1) and (j<=n) then
@@ -108,6 +118,8 @@ begin
     getFigureOn := 0;
   end;
 end;
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 function searchTo(i0,j0,dn,dm: integer): integer;
 var i,j: integer;
     current: integer;
@@ -127,6 +139,8 @@ begin
 	end;
 	searchTo := current;
 end;
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 function isUnderAttackByFigure(figure, i0, j0: integer): boolean;
 var res: boolean;
     i,j: integer;
@@ -247,12 +261,10 @@ begin
   		res := true;
   	end
   end;
-
   isUnderAttackByFigure := res;
-
-
 end;
-
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 function isUnderAttackBy(colorOfattacker, i0, j0: integer): boolean;
 var res: boolean;
 begin
@@ -279,6 +291,8 @@ begin
    end;
    isUnderAttackBy := res;
 end;
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 Procedure showField;
 var i,j: integer;
 begin
@@ -315,14 +329,135 @@ begin
       gotoxy(2+9*j,2+6*i);
       if (field[i,j]<>0) then write(abs(field[i,j]))
                          else write(' ');
-
     end;
   end;
-
+  readkey;
 end; // end showField
+procedure saveField;
+var f: text;
+    i,j: integer;
+begin
+  assign(f,'out.txt');
+  append(f);
+  for i:=1 to n do
+  begin
+    for j:=1 to n do
+    begin
+      write(f, field[i,j]:3);
+    end;
+    writeln(f);
+  end;
+  writeln(f);
+  close(f);
+end;
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+function CreateMove(iStart, jStart, iEnd, jEnd, figureStart: integer): move;
+var res: move;
+begin
+  res.iStart := iStart;
+  res.jStart := jStart;
+  res.iEnd := iEnd;
+  res.jEnd := jEnd;
+  res.figureStart := figureStart;
+  res.figureEnd := 0;
+  CreateMove := res;
+end;
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+procedure DoMove(var m: move);
+begin
+  with m do
+  begin
+    figureEnd := field[iEnd, jEnd];
+    field[iEnd, jEnd] := figureStart;
+    field[iStart, jStart] := 0;
+  end;
+end;
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+procedure UndoMove(var m: move);
+begin
+  with m do
+  begin
+    field[iStart, jStart] := figureStart;
+    field[iEnd, jEnd] := figureEnd;
+  end;
+end;
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+function isCheckTo(color: integer):boolean; //TOWRITE
+var res: boolean;
+begin
+  isCheckTo := res;
+end;
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+procedure AddAllPossibleMoves(var movarr: mas;color: integer);//TOWRITE
+begin
 
+end;
+function isMateTo(color: integer): boolean;
+var res: boolean;
+begin
+  res := true;
+  if (isCheckTo(color)) then
+  begin
+
+  end;
+  isMateTo := res;
+end;
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+procedure Solve(color, countOfMoves: integer);//TOWRITE
+var FirstPossibleMoveIndex: integer;
+    LastPossibleMoveIndex: integer;
+    i,j,k: integer;
+    checkWhite, checkBlack: boolean;
+    isOk: boolean;
+begin
+  if (countOfMoves > 0) then
+  begin
+    FirstPossibleMoveIndex := countOfPossibleMoves + 1;
+
+    isOk:= true;
+
+    checkWhite := isCheckTo(white);
+    checkBlack := isCheckTo(black);
+
+    if (checkWhite) and (isCheckToWhite) then isOk := false;
+    if (checkBlack) and (isCheckToBlack) then isOK := false;
+
+    if (isOk) then
+    begin
+      isCheckToWhite := checkWhite;
+      isCheckToBlack := checkBlack;
+      AddAllPossibleMoves(moves, color);
+      LastPossibleMoveIndex := countOfPossibleMoves;
+      if (LastPossibleMoveIndex < FirstPossibleMoveIndex) then
+      begin
+          showField;
+          saveField;
+      end else
+      for i := LastPossibleMoveIndex downto FirstPossibleMoveIndex do
+      begin
+        DoMove(moves[i]);
+        Solve(-color, countOfMoves -1);        
+        UndoMove(moves[i]);
+      end;
+      countOfPossibleMoves := FirstPossibleMoveIndex - 1;
+
+    end;
+  end else // countOfMoves <0
+  begin
+    if (isCheckTo(black)) then
+    begin
+      //showField;
+      //saveField;
+    end;
+  end;
+end;
 Begin
   initialize;
   showField;
-  readkey;
 end.
